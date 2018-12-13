d70d34d4164703a3c8d32018e47130bcf33f3647
A. Demant <ademant@ademant.de>
2018-12-13 06:21:48 +0100

    add grass armor for sun protection

--- config.lua
+++ config.lua
@@ -1,3 +1,7 @@
 for i,attr in ipairs({"exhaustion","hunger","thirst","sunburn","nyctophoby"}) do
 	physio_stress.attributes[attr]=minetest.settings:get("physio_stress."..attr) or false
 end
+
+if minetest.get_modpath("3d_armor") == nil then
+	physio_stress.attributes.sunburn = false
+end
--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 
 physio_stress = {}
-physio_stress.path = minetest.get_modpath("xpfw")
+physio_stress.path = minetest.get_modpath("physio_stress")
 physio_stress.modname=minetest.get_current_modname()
 physio_stress.mod_storage=minetest.get_mod_storage()
 physio_stress.store_table={}--xpfw.mod_storage:to_table()
@@ -10,8 +10,9 @@ minetest.log("action", "[MOD]"..minetest.get_current_modname().." -- start loadi
 -- Load files
 
 
-dofile(xpfw.path .. "/api.lua") -- API
-dofile(xpfw.path .. "/config.lua") -- API
-dofile(xpfw.path .. "/chat_commands.lua")
+--dofile(physio_stress.path .. "/api.lua") -- API
+dofile(physio_stress.path .. "/config.lua") -- API
+--dofile(physio_stress.path .. "/chat_commands.lua")
+dofile(physio_stress.path .. "/armor.lua")
 
 minetest.log("action", "[MOD]"..minetest.get_current_modname().." -- loaded ")
