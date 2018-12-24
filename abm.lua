03aad2c29f8898200c1111744e8483fa6010c237
A. Demant <ademant@ademant.de>
2018-12-24 09:22:45 +0100

    use armor in sunburn calc

--- abm.lua
+++ abm.lua
@@ -1,7 +1,6 @@
 minetest.register_globalstep(function(dtime)
 	physio_stress.dt=physio_stress.dt+dtime
 	if physio_stress.dt > physio_stress.dtime then
---		print("ping")
 		physio_stress.dt=0
 		local players = minetest.get_connected_players()
 		for i=1, #players do
@@ -10,12 +9,16 @@ minetest.register_globalstep(function(dtime)
 			local ps=physio_stress.player[name]
 			local act_pos=player:get_pos()
 			local act_light=minetest.get_node_light(act_pos)
---			print(dump2(physio_stress.attributes))
+			local player_armor=armor.def[name].count
 			if physio_stress.attributes.sunburn and act_light then
 				local player_meanlight=xpfw.player_get_attribute(player,"meanlight")
-				print("sunburn: "..(act_light-player_meanlight))
+--				print("sunburn: "..(act_light-player_meanlight))
 				if ((act_light-player_meanlight)>ps.sunburn_diff) then
-					player:set_hp( player:get_hp() - ps.sunburn_hp )
+					local diff_hp=ps.sunburn_hp
+					if player_armor>0 then
+						diff_hp=diff_hp*ps.sunburn_armor
+					end
+					player:set_hp( player:get_hp() - diff_hp )
 				end
 			end
 			if physio_stress.attributes.nyctophoby and act_light then
@@ -38,7 +41,12 @@ minetest.register_globalstep(function(dtime)
 					local player_meanlight=xpfw.player_get_attribute(player,"meanlight")
 					print("nycto: "..(player_meanlight-act_light))
 					if ((player_meanlight-act_light)>ps.nyctophoby_diff) then
-						player:set_hp( player:get_hp() - ps.nyctophoby_hp )
+						local diff_hp=ps.nyctophoby_hp
+						if player_armor>0 then
+							diff_hp=diff_hp*ps.nyctophoby_armor
+						end
+						player:set_hp( player:get_hp() - diff_hp )
+--						player:set_hp( player:get_hp() - ps.nyctophoby_hp )
 					end
 				end
 			end
