83b43cfd6de7029733494f7b3632730d2446e4e9
A. Demant <ademant@ademant.de>
2019-01-02 16:03:43 +0100

    code cleaning

--- physio_stress/api.lua
+++ physio_stress/api.lua
@@ -1,46 +1,94 @@
 
-minetest.register_on_joinplayer(function(player)
-	local playername = player:get_player_name()
-	if physio_stress.player[playername] == nil then
-		physio_stress.player[playername]=table.copy(physio_stress.default_player)
-	else
-		local ps=physio_stress.player[playername]
-		for i,attr in ipairs({"sunburn_diff","nyctophoby_diff","sunburn_hp","nyctophoby_hp","sunburn_armor","nyctophoby_armor"}) do
-			if ps[attr] == nil then
-				ps[attr]=minetest.settings:get("physio_stress."..attr) or 1
-			end
-		end
-		ps.saturation=20
-		ps.thirst=20
-	end
-	local ps=physio_stress.player[playername]
-	
-	ps.dug=xpfw.player_get_attribute(player,"dug")
-	ps.build=xpfw.player_get_attribute(player,"build")
-	ps.walked=xpfw.player_get_attribute(player,"walked")
-	ps.swam=xpfw.player_get_attribute(player,"swam")
-	physio_stress.hud_init(player)
-end)
 
 function physio_stress.hud_clamp(value)
-    if value < 0 then
-        return 0
-    elseif value > 20 then
-        return 20
-    else
-        return math.ceil(value)
-    end
+	if value == nil then
+		return 20
+	end
+	return math.max(0,math.min(20,value))
 end
-if minetest.get_modpath("hudbars") then
-    hb.register_hudbar('exhaust', 0xffffff, "Exhaustion", {
-        bar = 'physio_stress_hudbars_bar.png',
-        icon = 'physio_stress_exhaust_16.png'
-    }, 20, 20, false)
-    function physio_stress.hud_init(player)
-        hb.init_hudbar(player, 'exhaust',physio_stress.hud_clamp(xpfw.player_get_attribute(player, 'exhaustion')),
-			20, false)
-    end
-    function physio_stress.hud_update(player, value)
-        hb.change_hudbar(player, 'exhaust', physio_stress.hud_clamp(value), 20)
-    end
+
+function physio_stress.eat(hp_change, replace_with_item, itemstack, user, pointed_thing)
+	local item = itemstack:get_name()
+	local def = minetest.registered_items[item]
+	local thirst_change = 0
+	if not def then
+		def = {}
+		if type(hp_change) ~= "number" then
+			hp_change = 1
+			core.log("error", "Wrong on_use() definition for item '" .. item .. "'")
+		end
+		def.saturation = hp_change * 1.3
+		def.replace = replace_with_item
+		if not def.drink_hp then
+			thirst_change=tonumber(def.drink_hp) or 0
+		end
+	else
+		def={saturation=hp_change * 1.3,
+			replace=replace_with_item,
+			poisen = 0,
+			healing = 0,
+			sound="hbhunger_eat_generic",}
+	end
+	local func = physio_stress.item_eat(def.saturation, def.replace, def.poisen, def.healing, thirst_change,def.sound)
+	return func(itemstack, user, pointed_thing)
+end
+
+function physio_stress.item_eat(hunger_change, replace_with_item, heal, thirst_change,sound)
+	return function(itemstack, user, pointed_thing)
+		if itemstack:take_item() ~= nil and user ~= nil then
+			local name = user:get_player_name()
+			local ps=physio_stress.player[name]
+			local h = xpfw.player_get_attribute(user,"saturation")
+			local th = xpfw.player_get_attribute(user,"thirst")
+--			local h = tonumber(ps.saturation)
+--			local th = tonumber(ps.thirst)
+			local hp = user:get_hp()
+			if h == nil or hp == nil then
+				return
+			end
+			minetest.sound_play({name = sound or "hbhunger_eat_generic", gain = 1}, {pos=user:getpos(), max_hear_distance = 16})
+
+			-- Saturation
+			if hunger_change then
+				if hunger_change > 0 then
+					print("eat "..hunger_change)
+					xpfw.player_add_attribute(user,"saturation",hunger_change)
+				end
+			end
+			-- Thirst
+			if thirst_change then
+				if thirst_change > 0 then
+					xpfw.player_add_attribute(user,"thirst",thirst_change)
+				end
+			end
+			-- Healing
+			-- Poison
+
+			if itemstack:get_count() == 0 then
+				itemstack:add_item(replace_with_item)
+			else
+				local inv = user:get_inventory()
+				if inv:room_for_item("main", replace_with_item) then
+					inv:add_item("main", replace_with_item)
+				else
+					minetest.add_item(user:getpos(), replace_with_item)
+				end
+			end
+		end
+		return itemstack
+	end
+end
+
+function physio_stress.player_save(playername)
+	if playername == nil or playername == "" then
+		return
+	end
+	local def=physio_stress.player[playername]
+	for attr,val in pairs(def) do
+		if val ~= physio_stress.default_player[attr] then
+			physio_stress.mod_storage:set_float(playername.."_"..attr,val)
+		else
+	--		print(attr.." not changed")
+		end
+	end
 end
