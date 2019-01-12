6982eda728b5c8e5d0fc7641b2b91bb80d592ef6
A. Demant <ademant@ademant.de>
2019-01-12 17:22:07 +0100

    correct handling of exhaustion

--- physio_stress/abm.lua
+++ physio_stress/abm.lua
@@ -107,14 +107,9 @@ minetest.register_globalstep(function(dtime)
 				else
 					exh = 0
 				end
---				local act_exh=xpfw.player_get_attribute(player,"exhaustion")
 				-- if one speed excel actual exhaustion level than set to max.
---				if (exh > act_exh) then
-					xpfw.player_add_attribute(player,"exhaustion",exh)
---				elseif (act_exh > 0) and (act_exh>=(exh+1)) then
---					xpfw.player_sub_attribute(player,"exhaustion",1)
---				end
-				if exh > 19 then
+				xpfw.player_add_attribute(player,"exhaustion",exh)
+				if xpfw.player_get_attribute(player,"exhaustion") > 19 then
 					local ret = playereffects.apply_effect_type("exhausted", 60, player)
 				end
 				physio_stress.hud_update(player,"exhaustion",xpfw.player_get_attribute(player,"exhaustion"))
